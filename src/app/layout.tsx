a1a27aef8cdb3ddfc27ff7811c619e2f1abf4202
Waleed Habib <waleedhabib845@gmail.com>
2025-10-15 17:24:26 +0500

    speed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import Footer from "@/components/Footer";
 
 const geistSans = Geist({
@@ -30,6 +31,7 @@ export default function RootLayout({
         <Navbar />
         <main>
           {children}
+          <SpeedInsights />
         </main>
         <Footer />
       </body>
